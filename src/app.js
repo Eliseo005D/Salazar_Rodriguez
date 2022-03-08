4ddcbdd80b74a6df1f0b8607424da34b41e33fef
Eliseo_Salazar <godofwar_salazar@outlook.es>
2022-03-08 05:05:31 -0500

    Levantando server

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+
+import pkg from "../package.json";
+
+import productRoutes from "./routes/products.routes";
+import usersRoutes from "./routes/user.routes";
+import authRoutes from "./routes/auth.routes";
+
+import { createRoles, createAdmin} from "./libs/initialSetup";
+
+const app = express();
+createRoles();
+createAdmin();
+
+app.set("pkg", pkg);
+app.set("port", process.env.PORT || 4000);
+app.set("json spaces", 4);
+
+const corsOptions = {
+};
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Welcome to my Products API",
+    name: app.get("pkg").name,
+    version: app.get("pkg").version,
+    description: app.get("pkg").description,
+    author: app.get("pkg").author,
+  });
+});
+
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRoutes);
+
+export default app;
